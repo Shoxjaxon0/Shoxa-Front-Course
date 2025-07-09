251f7ea2a76240a76412c128fc89472ab25c1866
shoha <shoxjaxonaxmedov647@gmail.com>
2025-07-09 16:04:19 +0500

    classwork 09-07-2025

---
+++ copy-folder/js/script.js
@@ -0,0 +1,47 @@
+// let a = 123456;
+// let b = 12.44;
+// let c = 0.999;
+// let d = 2_555_777;
+
+// typeof a;
+
+// console.log(typeof (a + 1));
+// console.log(typeof (a + "hello" ));
+// console.log(1/0 * -1);
+// console.log("a" * -1);
+
+// let a = "a" + 1;
+// console.log(NaN * 1);
+// console.log(3 + 2);
+
+// let a = 67328783984598349398563495354n;
+// console.log(typeof a )
+ 
+// let str = "Это я";
+// let str1 = "Это я";
+// let str1 = 'I'm here';
+// let str1 = "I'm here";
+// console.log(str, str1);
+
+// ''
+// ""
+let age = 25
+let str1 = 'Мне ${age} лет';
+console.log(age);
+
+// let str1 = 'Это называется "строка"';
+// console.log( str1);
+// let str1 = "Мне ${age} лет";
+// console.log(typeof str1);
+// let a = true;
+// console.log(typeof a);
+// let a = 5 > 2
+// let a = 5 < 2;
+// console.log(a);
+
+// let a = null; 
+
+// console.log(typeof a)
+
+let a = undefined;
+console.log(a)
